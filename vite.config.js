bd27b63781ffe211b449e4f940af4a24b7fd2fd5
Mubi <150862765+cicerorph@users.noreply.github.com>
2026-03-23 11:18:23 -0300

    Atualizar o vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
-    build: {
-        copyPublicDir: false,
-        lib: {
-            entry: resolve(__dirname, 'src/main.js'),
-            name: 'Minecraft-Cape-Creator',
-            fileName: 'minecraft-cape-creator',
-        },
-        outDir: './dist'
-    }
+  build: {
+    outDir: './dist'
+  }
 })
